9aed5d7df0794a496bf079b41df5f681bf4d2add
TheBombre <carlton42@mailbox.org>
2021-03-09 10:20:29 +0200

    setup the editor layout

--- src/main.c
+++ src/main.c
@@ -1,22 +1,42 @@
+/**
+ * @file
+ * @brief Done so doxygen generates documentation for functions in this file
+ * */
 #include "main.h"
 
+// TODO: Ensure to check which functions return errors and handle these
+// appropriately
+// TODO: handle atexit method
 int main() {
   // Initialisation functions
   initscr();
   raw();
   noecho();
   keypad(stdscr, true);
+  refresh();
 
   // get screen size
   int rows, columns;
   getmaxyx(stdscr, rows, columns);
 
-  // Window* file_window = newwin()
-  int num = 0.9 * 41;
+  int file_window_rows = 0.92 * rows;
+  WINDOW *status_window =
+      newwin(rows - file_window_rows, columns, file_window_rows, 0);
+  WINDOW *file_window = newwin(file_window_rows, columns, 0, 0);
 
-  printw("Hello world: rows: %d, columns: %d\n", rows, columns);
-  printw("Number: %d", num);
-  refresh();
+  if (wborder(status_window, 0, 0, 0, 0, 0, 0, 0, 0) == ERR ||
+      wborder(file_window, 0, 0, 0, 0, 0, 0, 0, 0) == ERR) {
+    printf("Error occurred!");
+
+    exit(EXIT_FAILURE);
+  }
+  // wmove(file_window,1,1);
+  // wmove(status_window, 1, 1);
+  wborder(status_window, 0, 0, 0, 0, 0, 0, 0, 0);
+  mvwprintw(status_window, 0, columns / 2 - 5, " Status ");
+  mvwprintw(file_window, 0, columns / 2 - 5, " main.c ");
+  wrefresh(status_window);
+  wrefresh(file_window);
   getch();
   endwin();
   return EXIT_SUCCESS;
